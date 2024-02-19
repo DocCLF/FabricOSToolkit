f0897ad83389c27e4c09c051970370daa6c4611a
DocCLF <mailtodoc@web.de>
2024-02-19 19:46:51 +0100

    create some first files

---
+++ Scripts/FOS-IPAddress.ps1
@@ -0,0 +1,27 @@
+function FOS_IPAddrSet {
+    <#
+        .DESCRIPTION
+        Many text in here
+
+        .EXAMPLE
+        FOS_newIPAddrSet -ip 10120012
+        
+        .LINK
+        goggel if you can
+    #>
+    [CmdletBinding()]
+    param (
+        
+    )
+    Write-Host "FOSSometime is cool"
+    
+}
+
+function FOS_newIPAddrSet {
+    [CmdletBinding()]
+    param (
+        
+    )
+    Write-Host "FOSSometimeisnot is not cool"
+    
+}
