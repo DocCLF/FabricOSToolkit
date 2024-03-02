34bee05082c5a8fc823282d2448791bf816ff038
ToolDoc <mailtodoc@web.de>
2024-03-02 08:59:15 +0100

    Add some new commands, fixed some bugs after testing

--- Scripts/FOS-Basic-Commad-Collection.ps1
+++ Scripts/FOS-Basic-Commad-Collection.ps1
@@ -1,61 +1,180 @@
+using namespace System.Net
 
 function FOS_NTP_Server {
-        <#
-        .DESCRIPTION
-        Displays or sets the Network Time Protocol (NTP) Server addresses.
 
-        .EXAMPLE
-        To find an Alias by his exact name 
-        FOS_Alias_Details -UserName admin -SwitchIP 10.10.10.30 -AliasName Storage_N1_P2
+    <#
+    .DESCRIPTION
+    Displays or sets the Network Time Protocol (NTP) Server addresses.
 
-        or by a wildcard
-        FOS_Alias_Details -UserName admin -SwitchIP 10.10.10.30 -AliasName Storage*
+    .EXAMPLE
+    To display the default clock server:
+    FOS_NTP_Server -UserName admin -SwitchIP 10.10.10.30
+    
+    To set the NTP server to a specified IP address:
+    FOS_NTP_Server -UserName admin -SwitchIP 10.10.10.30 -NTP_Server 10.10.20.30
 
-        To get a list of all aliases with WWPN
-        FOS_Alias_Details -UserName admin -SwitchIP 10.10.10.30
+    To display NTP server authentication state:
+    FOS_NTP_Server -UserName admin -SwitchIP 10.10.10.30 -NTP_Show show
 
-        .LINK
-        Brocade® Fabric OS® Command Reference Manual, 9.2.x
-        https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
-        #>
-        param (
-            [Parameter(Mandatory,ValueFromPipeline)]
-            [string]$UserName,
-            [Parameter(Mandatory,ValueFromPipeline)]
-            [ipaddress]$SwitchIP,
-            [Parameter(ValueFromPipeline)]
-            [ipaddress]$NTP_Server,
-            [Parameter(ValueFromPipeline)]
-            [ValidateSet("show","showkeys")]
-            [string]$NTP_Show
-        )
+    To display NTP Authentication keys:
+    FOS_NTP_Server -UserName admin -SwitchIP 10.10.10.30 -NTP_Show showkeys
+
+    .LINK
+    Brocade® Fabric OS® Command Reference Manual, 9.2.x
+    https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
+    #>
+
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ipaddress]$SwitchIP,
+        [Parameter(ValueFromPipeline)]
+        [ipaddress]$NTP_Server,
+        [Parameter(ValueFromPipeline)]
+        [ValidateSet("show","showkeys")]
+        [string]$NTP_Show
+    )
         
-        begin{
-            Write-Debug -Message "Begin block $(Get-Date)"
-            Write-Debug -Message "NTP-Server $NTP_Server, NTP-Show $NTP_Show | $(Get-Date)"
-            if((($NTP_Server) -and $NTP_Show) -ne "" ){
-                Write-Host "It is not permitted to use the -NTP_Show parameter in combination with the -NTP_Server parameter!" -ForegroundColor Red
-                break
-            }else{
-                Write-Debug -Message "No error found between server and show"
-            }
+    begin{
+        Write-Debug -Message "Begin block $(Get-Date)"
+        Write-Debug -Message "NTP-Server $NTP_Server, NTP-Show $NTP_Show | $(Get-Date)"
+        if((($NTP_Server) -and $NTP_Show) -ne "" ){
+            Write-Host "It is not permitted to use the -NTP_Show parameter in combination with the -NTP_Server parameter!" -ForegroundColor Red
+            break
+        }else{
+            Write-Debug -Message "No error found between server and show"
         }
-        process{
-            Write-Debug -Message "Process block $(Get-Date)"
+    }
+    process{
+        Write-Debug -Message "Process block $(Get-Date)"
 
-            if(($NTP_Server -eq "") -or ($NTP_Show -notlike "show*")){
-                $endResult = ssh $UserName@$($SwitchIP) "tsclockserver"
-            }
-            elseif($NTP_Show -notlike "show*"){
-                $endResult = ssh $UserName@$($SwitchIP) "tsclockserver ""$NTP_Server"" "
-            }else {
-                $endResult = ssh $UserName@$($SwitchIP) "tsclockserver --$NTP_Show "
-            }
-            Write-Debug -Message "$endResult"
+        if(($NTP_Server -eq "") -or ($NTP_Show -notlike "show*")){
+            $endResult = ssh $UserName@$($SwitchIP) "tsclockserver"
         }
-        end{
+        elseif($NTP_Show -notlike "show*"){
+            $endResult = ssh $UserName@$($SwitchIP) "tsclockserver ""$NTP_Server"" "
+        }else {
+            $endResult = ssh $UserName@$($SwitchIP) "tsclockserver --$NTP_Show "
+        }
+        Write-Debug -Message "$endResult"
+    }
+    end{
             Write-Debug -Message "End block $(Get-Date)"
             Clear-Variable NTP_Se* -Scope Local;
             Write-Host "$endResult" -ForegroundColor Green
+    }    
+}
+
+function FOS_Set_Sw_Ch_Names {
+    <#
+    .DESCRIPTION
+    Displays or sets the Network Time Protocol (NTP) Server addresses.
+
+    .EXAMPLE
+    Displays the switch & chassis name.
+    FOS_Set_Sw_Ch_Names -UserName admin -SwitchIP 10.10.10.30
+    
+    Sets the switch and chassis name.
+    FOS_Set_Sw_Ch_Names -UserName admin -SwitchIP 10.10.10.30 -switchname DMZ_Switch01 -chassisname Prod_Chassis_11
+
+    To set the switch name.
+    FOS_Set_Sw_Ch_Names -UserName admin -SwitchIP 10.10.10.30 -switchname LabSwitch01
+
+    To set the chassis name.
+    FOS_Set_Sw_Ch_Names -UserName admin -SwitchIP 10.10.10.30 -chassisname TestChassis_11
+
+    .LINK
+    Brocade® Fabric OS® Command Reference Manual, 9.2.x
+    https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
+    #>
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ipaddress]$SwitchIP,
+        [Parameter(ValueFromPipeline)]
+        [string]$SwitchName,
+        [Parameter(ValueFromPipeline)]
+        [string]$ChassisName
+    )
+    begin{
+        Write-Debug -Message "Begin block $(Get-Date)"
+    }
+    process{
+        Write-Debug -Message "Process block $(Get-Date)"
+
+        $endResult = ssh $UserName@$($SwitchIP) "switchname $SwitchName && chassisname $ChassisName "
+
+        Write-Debug -Message "$endResult"
+    }
+    end{
+            Write-Debug -Message "End block $(Get-Date)"
+            Write-Host "$endResult" -ForegroundColor Green
+    }    
+    
+}
+
+function FOS_Fabric_Names {
+    <#
+    .DESCRIPTION
+    Configures fabric name and displays the fabric name parameter.
+
+    .EXAMPLE
+    To display the fabric name:
+    FOS_Fabric_Names -UserName admin -SwitchIP 10.10.10.30 -Operand show
+    
+    To set fabric name:
+    FOS_Fabric_Names -UserName admin -SwitchIP 10.10.10.30 -Operand set -FabricName newfabric
+
+    To clear the fabric name already set:
+    FOS_Fabric_Names -UserName admin -SwitchIP 10.10.10.30 -Operand clear
+
+    .LINK
+    Brocade® Fabric OS® Command Reference Manual, 9.2.x
+    https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
+    #>
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ipaddress]$SwitchIP,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ValidateSet("show","set","clear")]
+        [string]$Operand,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$FabricName
+    )
+    begin{
+        Write-Debug -Message "Begin block $(Get-Date)"
+
+        if((($Operand -eq "show") -or ($Operand -eq "clear")) -and ($FabricName -ne "")){
+            Write-Host "$Operand does not work in combination with $FabricName " -ForegroundColor Red
+            Write-Debug -Message "$Operand and $FabricName are set, leave the func | $(Get-Date)"
+            break
         }
+    }
+    process{
+        Write-Debug -Message "Process block $(Get-Date)"
+
+        switch ($Operand) {
+            "show" { 
+                $endResult = ssh $UserName@$($SwitchIP) "fabricname --$Operand " 
+            }
+            "clear" { 
+                $endResult = ssh $UserName@$($SwitchIP) "fabricname --$Operand " 
+            }
+            "set" { 
+                $endResult = ssh $UserName@$($SwitchIP) "fabricname --$Operand $FabricName" 
+            }
+            Default {}
+        }
+
+        Write-Debug -Message "$endResult"
+    }
+    end{
+            Write-Debug -Message "End block $(Get-Date)"
+            Write-Host "$endResult" -ForegroundColor Green
+    }    
+    
 }
---
+++ Scripts/FOS-SwitchDetails.ps1
@@ -0,0 +1,118 @@
+using namespace System.Net
+
+function FOS_Switch_Health_Status {
+        <#
+        .DESCRIPTION
+        Displays the dashboard showing an at-a-glance snapshot of switch health status.
+
+        .EXAMPLE
+        Displays a summary of the data collected since midnight of the current day.
+        FOS_Switch_Health_Status -UserName admin -SwitchIP 10.10.10.25
+        
+        Displays a summary and historical data of the errors for 5 rules and last 5 ports.
+        FOS_Switch_Health_Status -UserName admin -SwitchIP 10.10.10.25 -Operand all -CreateExportFile yes -ExportFile C:\Temp\
+        
+        Displays the historical data only.
+        FOS_Switch_Health_Status -UserName admin -SwitchIP 10.10.10.25 -Operand history
+        
+
+        .LINK
+        Brocade® Fabric OS® Command Reference Manual, 9.1.x
+        https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-1-x/Fabric-OS-Commands.html
+        #>
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ipaddress]$SwitchIP,
+        [Parameter(ValueFromPipeline)]
+        [ValidateSet("all","history")]
+        [string]$Operand,
+        [Parameter(ValueFromPipeline)]
+        [ValidateSet("yes","no")]
+        [string]$CreateExportFile = "no",
+        [Parameter(ValueFromPipeline)]
+        [string]$ExportFile
+    )
+    begin{
+        Write-Debug -Message "Begin block |$(Get-Date)"
+        $OnlineDevice="SwitchStatus-$(Get-Date -Format "yyyy-MM-HH-mm-ss").csv"
+        If($CreateExportFile -eq "yes"){
+            Write-Host "The default path for the export is $Env:TEMP, if you want to specify your own, please enter it below."
+            $ExportFile = Read-Host "Enter a path to the folder for the export "
+            if($ExportFile -eq ""){$ExportFile = $Env:TEMP}
+        }
+        
+    }
+    process{
+        Write-Debug -Message "Process block |$(Get-Date)"
+        $result = ssh $UserName@$($SwitchIP) mapsdb --show $Operand
+        Start-Sleep -Seconds 3;
+        Write-Host "Here the Switch Health Report:`n" -ForegroundColor Green
+        $result
+        If($CreateExportFile -eq "yes"){
+        Export-Csv -Path $ExportFile\$OnlineDevice -InputObject $result -NoTypeInformation
+        }
+    }
+    end{
+        Clear-Variable -Name result;
+        Write-Debug -Message "End block |$(Get-Date)"
+    }
+}
+
+function FOS_Switch_SensorShow {
+    <#
+    .DESCRIPTION
+    Use this command to display the current temperature, fan, and power supply status and readings from sensors located on the switch. 
+    The actual location of the sensors varies, depending on the switch type.
+
+    .EXAMPLE
+    Displays the output without saving to a file
+    FOS_Switch_SensorShow -UserName admin -SwitchIP 10.10.10.25
+
+    Displays the output with saving the data in a specific path.
+    FOS_Switch_SensorShow -UserName admin -SwitchIP 10.10.10.25 -CreateExportFile yes -ExportFile C:\Temp\
+
+    Displays the output with saving the data in a *. csv file
+    FOS_Switch_SensorShow -UserName admin -SwitchIP 10.10.10.25 -CreateExportFile yes
+
+    .LINK
+    Brocade® Fabric OS® Command Reference Manual, 9.1.x
+    https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-1-x/Fabric-OS-Commands.html
+    #>
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$UserName,
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [ipaddress]$SwitchIP,
+        [Parameter(ValueFromPipeline)]
+        [ValidateSet("yes","no")]
+        [string]$CreateExportFile = "no",
+        [Parameter(ValueFromPipeline)]
+        [string]$ExportFile
+    )
+    begin{
+        Write-Debug -Message "Begin block |$(Get-Date)"
+        $OnlineDevice="SensorShow-$(Get-Date -Format "yyyy-MM-HH-mm-ss").csv"
+        If($CreateExportFile -eq "yes"){
+            Write-Host "The default path for the export is $Env:TEMP, if you want to specify your own, please enter it below."
+            $ExportFile = Read-Host "Enter a path to the folder for the export "
+            if($ExportFile -eq ""){$ExportFile = $Env:TEMP}
+        }
+        
+    }
+    process{
+        Write-Debug -Message "Process block |$(Get-Date)"
+        $result = ssh $UserName@$($SwitchIP) "sensorshow"
+        Start-Sleep -Seconds 3;
+        Write-Host "Here the Sensor Show Report:`n" -ForegroundColor Green
+        $result
+        If($CreateExportFile -eq "yes"){
+        Export-Csv -Path $ExportFile\$OnlineDevice -InputObject $result -NoTypeInformation
+        }
+    }
+    end{
+        Clear-Variable -Name result;
+        Write-Debug -Message "End block |$(Get-Date)"
+    }
+}
